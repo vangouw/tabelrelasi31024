387c8ae97d0af65c350db62593d48f36579a93fe
vangouw <140374004+vangouw@users.noreply.github.com>
2024-10-07 10:44:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ClassController;
 
+Route::get('/', function () {
+    return redirect()->route('students.index');
+});
+
 Route::get('/classes', [ClassController::class, 'index'])->name('classes.index');
 
 Route::resource('students', StudentController::class);
+
